e46d0ecbf4088e415ef37f9a34e2067549463781
Willie <mulwawillie@gmail.com>
2024-05-04 02:31:01 +0300

    First commit

---
+++ Fake news detection.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import json
+import csv
+import random
+
+from tensorflow.keras.preprocessing.text import tokenizers
+from tensorflow.keras.preprocessing.sequence import pad_sequences
