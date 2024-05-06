9e76d9791cecde97769cf974e2e3adf576f710ed
Willie <mulwawillie@gmail.com>
2024-05-06 11:55:53 +0300

    First commit

--- Fake news detection.py
+++ Fake news detection.py
@@ -7,3 +7,7 @@ import random
 from tensorflow.keras.preprocessing.text import tokenizers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras import preprocessing
+tf.disable_eager_execution()
+data = pd.read_csv('Fake news.csv')
+data.head()
