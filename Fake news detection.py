5b8d4d296d5d7860a4157ac83bd69c6245cb1c37
Willie <mulwawillie@gmail.com>
2024-05-16 18:49:10 +0300

    updating files

--- Fake news detection.py
+++ Fake news detection.py
@@ -1,13 +1,17 @@
-import numpy as np
+import tweepy
 import pandas as pd
-import json
-import csv
-import random
 
-from tensorflow.keras.preprocessing.text import tokenizers
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras import preprocessing
-tf.disable_eager_execution()
-data = pd.read_csv('Fake news.csv')
-data.head()
+consumer_key = 'Ncq55A71gRVigy6mXZ7JTq4kC'
+consumer_secret = 'Tz0MChm2q7Dt6rTOES9NWIV0xIBTJqLvmcJOVygu5B9hEyP7EP'
+access_token = '725028370522710017-2LQG8idx0i1kDmTpq8YZIO5vxIm9PH0'
+access_token_secret = 'dM0gP1sXOiUHbbJFaonvkvenFnR83x7aZkNPH8FDV4pi'
+
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+def fetch_tweets(query, count=100):
+    tweets = tweepy.Cursor(api.search_tweets, q=query, lang="en").items(count)
+    tweet_list = [[tweet.text] for tweet in tweets]
+    return pd.DataFrame(tweet_list, columns=['tweet'])
+data = fetch_tweets('COVID-19 vaccine', count=200)
+print(data.head())
